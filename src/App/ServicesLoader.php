762de22df764464b06b8f6432648a3468c67856b
matsu-chara <matsuy00@gmail.com>
2014-01-12 02:20:58 +0900

    sample

---
+++ src/App/ServicesLoader.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Silex\Application;
+
+class ServicesLoader
+{
+    protected $app;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function bindServicesIntoContainer()
+    {
+    }
+}
+
